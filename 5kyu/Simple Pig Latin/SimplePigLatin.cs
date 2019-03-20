ebb76e99477a07a442edc05ddce83a52dd40480c
david.huang <guohuang107@gmail.com>
2019-03-20 23:14:25 +0800

    add 5kyu and simplePigLatin

---
+++ 5kyu/Simple Pig Latin/SimplePigLatin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _5kyu
+{
+    class SimplePigLatin
+    {
+
+        public static string PigIt(string str)
+        {
+            var words = str.Split(" ");
+            string result = "";
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrEmpty(result))
+                {
+                    result += " ";
+                }
+                result += $"{word.Substring(1, word.Length-1)}{word.Substring(0,1)}ay";
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ 5kyu/Simple Pig Latin/SimplePigLatinUnitTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _5kyu
+{
+    [TestClass]
+    public class SimplePigLatinUnitTest
+    {
+        [TestMethod]
+        [DataRow("Pig latin is cool", "igPay atinlay siay oolcay")]
+        [DataRow("This is my string", "hisTay siay ymay tringsay")]
+        public void PigIt_should_success(string input, string expected)
+        {
+            Assert.AreEqual(expected, SimplePigLatin.PigIt(input));
+
+        }
+    }
+}
