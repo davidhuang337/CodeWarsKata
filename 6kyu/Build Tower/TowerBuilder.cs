9738bca84dcfd16af79fbad5c7a71f64c230672d
david.huang <guohuang107@gmail.com>
2019-05-25 21:18:36 +0800

    add TowerBuilderTest and TowerBuilder

---
+++ 6kyu/Build Tower/TowerBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _6kyu.Split_Strings
+{
+    public class TowerBuilder
+    {
+        public static string[] Build(int nFloors)
+        {
+            var result = Enumerable.Range(0, nFloors)
+                .Select(floor => new string(' ',nFloors-floor-1) + new string('*',2*floor+1) + new string(' ', nFloors-floor-1) )
+                .ToArray();
+            return result;
+        }
+    }
+}
---
+++ 6kyu/Build Tower/TowerBuilderTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _6kyu.Split_Strings
+{
+    [TestClass]
+    public class TowerBuilderTest
+    {
+        [TestMethod]
+        public void BasicTests()
+        {
+            Assert.AreEqual(string.Join(",", new[] { "*" }), string.Join(",", TowerBuilder.Build(1)));
+            Assert.AreEqual(string.Join(",", new[] { " * ", "***" }), string.Join(",", TowerBuilder.Build(2)));
+            Assert.AreEqual(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", TowerBuilder.Build(3)));
+        }
+
+
+    }
+}
